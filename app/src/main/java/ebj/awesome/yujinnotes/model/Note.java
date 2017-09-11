3505888acd89c25cd95825d62ddd4e252351bdc9
Yudzeen <eugenejavinas@gmail.com>
2017-09-11 12:18:12 +0800

    Added note model

---
+++ app/src/main/java/ebj/awesome/yujinnotes/model/Note.java
@@ -0,0 +1,32 @@
+package ebj.awesome.yujinnotes.model;
+
+/**
+ * Created by Yujin on 11/09/2017.
+ */
+
+public class Note {
+
+    private String title;
+    private String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
