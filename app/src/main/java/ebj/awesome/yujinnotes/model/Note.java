9b75392bef4a00c6ecc15917b73bcd43be2709d4
Yudzeen <eugenejavinas@gmail.com>
2017-09-13 13:26:44 +0800

    Added id attribute

--- app/src/main/java/ebj/awesome/yujinnotes/model/Note.java
+++ app/src/main/java/ebj/awesome/yujinnotes/model/Note.java
@@ -1,20 +1,38 @@
 package ebj.awesome.yujinnotes.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Yujin on 11/09/2017.
  */
 
-public class Note {
+public class Note implements Parcelable {
+
+    public static final String TAG = Note.class.getName();
 
     public static final String TITLE = "title";
     public static final String DESCRIPTION = "desc";
 
+    public static int ID_COUNT = 0;
+
+    private int id;
     private String title;
     private String description;
 
     public Note(String title, String description) {
         this.title = title;
         this.description = description;
+        this.id = ID_COUNT;
+        ID_COUNT++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -32,4 +50,38 @@ public class Note {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    /**Parcel**/
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public Note createFromParcel(Parcel source) {
+            return new Note(source);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    public Note(Parcel source) {
+        id = source.readInt();
+        title = source.readString();
+        description = source.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(description);
+    }
+
+
 }
