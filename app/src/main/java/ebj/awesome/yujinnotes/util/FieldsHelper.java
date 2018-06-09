1a62f53622f0ecec12d774cff20cb9cb2c98fbf6
Eugene Javiñas <eugenejavinas@gmail.com>
2018-06-09 19:26:42 +0800

    Added yujin pogi

--- app/src/main/java/ebj/awesome/yujinnotes/util/FieldsHelper.java
+++ app/src/main/java/ebj/awesome/yujinnotes/util/FieldsHelper.java
@@ -3,7 +3,7 @@ package ebj.awesome.yujinnotes.util;
 import android.widget.EditText;
 
 /**
- * Created by Yujin on 25/09/2017.
+ * Created by Yujin Pogi on 25/09/2017.
  */
 
 public class FieldsHelper {
